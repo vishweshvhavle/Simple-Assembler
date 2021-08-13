d1f9a52e7e8d61cdaa0f8770d06c646e7f7b7db4
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2021-08-13 13:44:37 +0530

    New Workflow

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -3,154 +3,28 @@
 #Dev Thakkar, 2020052
 #Sahil Deshpande, 2020114
 
-#ISA OPERATIONS
+import operations.py
+import errors.py
 
-#OPCODES for quick reference
-# add 00000 | sub 00001 | mov $Imm 00010 | mov 00011 | ld 00100 | st 00101 | mul 00110 | div 00111 | rs 01000 | ls 01001 | xor 01010 | or 01011 | and 01100
-# not 01101 | cmp 01110 | jmp 01111 | jlt 10000 | jgt 10001 | je 10010 | hlt 10011  
+#OPCODES for reference
+# add 00000 | sub 00001 | mov $Imm 00010 | mov 00011 | ld 00100 | st 00101 
+# mul 00110 | div 00111 | rs 01000 | ls 01001 | xor 01010 | or 01011 | and 01100
+# not 01101 | cmp 01110 | jmp 01111 | jlt 10000 | jgt 10001 | je 10010 | hlt 10011 
 
-#we take input in various lines and store individual lines in a array
-arr = []
-while True:
-    operation = input()
-    arr.append(operation)
-    if operation == 'hlt':
-        break
+#FLAGS
+bool V = false
 
-#empty string for output
+#Empty string for output
 output = ""
 
-#Performs reg1 = reg2 + reg3. If the computation
-#overflows, then the overflow flag is set
 
-def add():
-    if operation == 'ADD':
-        output = output + '00000' 
-    return
+#We take input in various lines and store individual lines in a array
+while True:
+    operation = input()
 
-
-#Performs reg1 = reg2 - reg3. In case reg3 > reg2,
-#0 is written to reg1 and overflow flag is set.
-
-def sub():
-    return
+    if operation == 'hlt':
+      break
 
 
-#Performs reg1 = $Imm where Imm is a 8 bit value.
-
-def moveImmediate():
-    return
 
 
-#Performs reg1 = reg2.
-
-def moveRegister():
-    return
-
-
-#Loads data from mem_addr into reg1.
-
-def load():
-    return
-
-
-#Stores data from reg1 to mem_addr.
-
-def store():
-    return
-
-
-#Performs reg1 = reg2 x reg3. If the computation
-#overflows, then the overflow flag is set.
-
-def mul():
-    return
-
-
-#Performs reg3/reg4. Stores the quotient
-#in R0 and the remainder in R1.
-
-def div():
-    return
-
-
-#Right shifts reg1 by $Imm,
-#where $Imm is an 8 bit value.
-
-def rightShift():
-    return
-
-
-#Left shifts reg1 by $Imm,
-#where $Imm is an 8 bit value.
-
-def leftShift():
-    return
-
-
-#Performs bitwise XOR of reg2 and reg3.
-#Stores the result in reg1.
-
-def xor():
-    return
-
-
-#Performs bitwise OR of reg2 and reg3.
-#Stores the result in reg1.
-
-def Or():
-    return
-
-
-#Performs bitwise AND of reg2 and reg3.
-#Stores the result in reg1.
-
-def And():
-    return
-
-
-#Performs bitwise NOT of reg2.
-#Stores the result in reg1.
-
-def invert():
-    return
-
-
-#Compares reg1 and reg2 and sets up the FLAGS register.
-
-def compare():
-    return
-
-
-#Jumps to mem_addr, where mem_addr is a memory address.
-
-def jmp():
-    return
-
-
-#Jump to mem_addr if the less than flagis set (less than
-#flag = 1), where mem_addr is a memory address.
-
-def jlt():
-    return
-
-
-#Jump to mem_addr if the greater than flag is set (greater than
-#flag = 1), where mem_addr is a memory address.
-
-def jgt():
-    return
-
-
-#Jump to mem_addr if the equal flag is set (equal flag =
-#1), where mem_addr is a memory address.
-
-def je():
-    return
-
-#Stops the machine from executing until reset
-
-def halt():
-    return
-
-#ERRORS
---
+++ Simple-Assembler/errors.py
@@ -0,0 +1,43 @@
+#ERRORS
+
+#Typos in instruction name or register name
+def error_a():
+    return
+
+#Use of undefined variables
+def error_b():
+    return
+
+#Use of undefined labels
+def error_c():
+    return
+
+#Illegal use of FLAGS register
+def error_d():
+    return
+
+#Illegal Immediate values (less than 0 or more than 255)
+def error_e():
+    return
+
+#Misuse of labels as variables or vice-versa
+def error_f():
+    return
+
+#Variables not declared at the beginning
+def error_g():
+    return
+
+#Missing hlt instruction
+def error_h():
+    return
+
+#hlt not being used as the last instruction
+def error_i():
+    return
+
+#Wrong syntax used for instructions (For example, add 
+#instruction being used as a type B instruction )
+def error_j():
+    return
+
