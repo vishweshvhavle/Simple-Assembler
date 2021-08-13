2898517f0154334466a8ffda1a2c2b53f77b255f
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2021-08-13 21:36:39 +0530

    Update #1

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -13,12 +13,11 @@ from sys import stdin
 # not 01101 | cmp 01110 | jmp 01111 | jlt 10000 | jgt 10001 | je 10010 | hlt 10011
 
 #FLAGS
-V = False #Overflow
-L = False #Less than in comparator
-G = False #Greater than in comparator
-E = False #Equal to in comparator
+FLAG = 0000000000000000
 
 #-----------------------------------------------------------------------------------
+
+no_vars = 0 #number of variables
 
 #Storing stdin inputs of assembly code in array
 assembly_code = []
@@ -35,16 +34,66 @@ for line in assembly_code:
         if line[0] == 'add':
             machine_code.append(operations.add(line[1],line[2],line[3],line_number))
 
-        if line[0] == 'sub':
+        elif line[0] == 'sub':
             machine_code.append(operations.sub(line[1],line[2],line[3],line_number))
 
-        if line[0] == 'add':
-            machine_code.append(operations.add(line[1],line[2],line[3],line_number))
+        elif line[0] == 'mov':
+            if line[2][0] == '$':
+                machine_code.append(operations.moveImmediate(line[1],line[2][1:],line_number))
+            else:
+                machine_code.append(operations.moveRegister(line[1],line[2],line_number))
 
-        if line[0] == 'add':
-            machine_code.append(operations.add(line[1],line[2],line[3],line_number))
+        elif line[0] == 'ld':
+            machine_code.append(operations.load(line[1],line[2],line_number))
 
+        elif line[0] == 'st':
+            machine_code.append(operations.store(line[1],line[2],line_number))
 
+        elif line[0] == 'mul':
+            machine_code.append(operations.mul(line[1],line[2],line[3],line_number))
 
+        elif line[0] == 'div':
+            machine_code.append(operations.div(line[1],line[2],line[3],line_number))
 
+        elif line[0] == 'rs':
+            machine_code.append(operations.rightShift(line[1],line[2],line[3],line_number))
 
+        elif line[0] == 'ls':
+            machine_code.append(operations.leftShift(line[1],line[2],line[3],line_number))
+
+        elif line[0] == 'xor':
+            machine_code.append(operations.xor(line[1],line[2],line[3],line_number))
+
+        elif line[0] == 'or':
+            machine_code.append(operations.Or(line[1],line[2],line[3],line_number))
+
+        elif line[0] == 'not':
+            machine_code.append(operations.invert(line[1],line[2],line_number))
+
+        elif line[0] == 'cmp':
+            machine_code.append(operations.compare(line[1],line[2],line_number))
+
+        elif line[0] == 'jmp':
+            machine_code.append(operations.jmp(line[1],line_number))
+
+        elif line[0] == 'jlt':
+            machine_code.append(operations.jlt(line[1],line_number))
+
+        elif line[0] == 'jgt':
+            machine_code.append(operations.jgt(line[1],line_number))
+
+        elif line[0] == 'je':
+            machine_code.append(operations.je(line[1],line_number))
+
+        elif line[0] == 'hlt':
+            if line_number < len(assembly_code)-1:
+                errors.error_i()
+                break
+            machine_code.append(operations.hlt())
+    else:
+        continue
+
+    line_number = line_number + 1
+
+for line in machine_code:
+    print(line)
--- Simple-Assembler/operations.py
+++ Simple-Assembler/operations.py
@@ -5,138 +5,166 @@ import errors
 # OPCODES for reference
 # add 00000 | sub 00001 | mov $Imm 00010 | mov 00011 | ld 00100 | st 00101
 # mul 00110 | div 00111 | rs 01000 | ls 01001 | xor 01010 | or 01011 | and 01100
-# not 01101 | cmp 01110 | jmp 01111 | jlt 10000 | jgt 10001 | je 10010 | hlt 10011
+# invert 01101 | cmp 01110 | jmp 01111 | jlt 10000 | jgt 10001 | je 10010 | hlt 10011
 
 #-----------------------------------------------------------------------------------
 
+def regFind(register):
+    if register == 'R0':
+        return '000'
+    elif register == 'R1':
+        return '001'
+    elif register == 'R2':
+        return '010'
+    elif register == 'R2':
+        return '011'
+    elif register == 'R4':
+        return '100'
+    elif register == 'R5':
+        return '101'
+    elif register == 'R6':
+        return '110'
+    elif register == 'FLAGS':
+        return '111'
+    else:
+        errors.error_a()
+
+def binary(num):
+    num = int(num)
+    bnr = bin(num).replace('0b','')
+    x = bnr[::-1]
+    while len(x) < 8:
+        x += '0'
+    bnr = x[::-1]
+    return bnr
+
 # Performs reg1 = reg2 + reg3. If the computation
 # overflows, then the overflow flag is set
-
-def add():
-    return
-
+# A | opcode (unused 2bits) reg1 reg2 reg3
+def add(reg1, reg2, reg3, line_number):
+    return '00000' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 # Performs reg1 = reg2 - reg3. In case reg3 > reg2,
 # 0 is written to reg1 and overflow flag is set.
-
-def sub():
-    return
+# A |opcode (unused 2bits) reg1 reg2 reg3
+def sub(reg1, reg2, reg3, line_number):
+    return '00001' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 
 # Performs reg1 = $Imm where Imm is a 8 bit value.
-
-def moveImmediate():
-    return
+# B | opcode(5bits) reg1 immediatevalue(8bits)
+def moveImmediate(reg1, value, line_number):
+    return '00010' + regFind(reg1) + binary(int(value))
 
 
 # Performs reg1 = reg2.
-
-def moveRegister():
-    return
+# C | opcode unused(5bits) reg1 reg2
+def moveRegister(reg1, reg2, line_number):
+    return '00011' + '00000' + regFind(reg1) + regFind(reg2)
 
 
 # Loads data from mem_addr into reg1.
-
-def load():
-    return
+# D | opcode reg1 memory(8bits)
+def load(reg1, value, line_number):
+    return '00100' + regFind(reg1) + binary(int(value))
 
 
 # Stores data from reg1 to mem_addr.
-
-def store():
-    return
+# D | opcode reg1 memory(8bits)
+def store(reg1, value, line_number):
+    return '00101' + regFind(reg1) + binary(int(value))
 
 
 # Performs reg1 = reg2 x reg3. If the computation
 # overflows, then the overflow flag is set.
-
-def mul():
-    return
+# A |opcode (unused 2bits) reg1 reg2 reg3
+def mul(reg1, reg2, reg3, line_number):
+    return '00110' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 
 # Performs reg3/reg4. Stores the quotient
 # in R0 and the remainder in R1.
-
-def div():
-    return
+# C | opcode unused(5bits) reg1 reg2
+def div(reg1, reg2, line_number):
+    return '00111' + '00000' + regFind(reg1) + regFind(reg2)
 
 
 # Right shifts reg1 by $Imm,
 # where $Imm is an 8 bit value.
-
-def rightShift():
-    return
+# B | opcode(5bits) reg1 immediatevalue(8bits)
+def rightShift(reg1, value,line_number):
+    return '01000'
 
 
 # Left shifts reg1 by $Imm,
 # where $Imm is an 8 bit value.
-
-def leftShift():
-    return
+# B | opcode(5bits) reg1 immediatevalue(8bits)
+def leftShift(reg1, value,line_number):
+    return '01001'
 
 
 # Performs bitwise XOR of reg2 and reg3.
 # Stores the result in reg1.
-
-def xor():
-    return
+# A |opcode (unused 2bits) reg1 reg2 reg3
+def xor(reg1, reg2, reg3, line_number):
+    return '01010' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 
 # Performs bitwise OR of reg2 and reg3.
 # Stores the result in reg1.
-
-def Or():
-    return
+# A |opcode (unused 2bits) reg1 reg2 reg3
+def Or(reg1, reg2, reg3, line_number):
+    return '01011' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 
 # Performs bitwise AND of reg2 and reg3.
 # Stores the result in reg1.
-
-def And():
-    return
+# A |opcode (unused 2bits) reg1 reg2 reg3
+def And(reg1, reg2, reg3, line_number):
+    return '01100' + '00' + regFind(reg1) + regFind(reg2) + regFind(reg3)
 
 
 # Performs bitwise NOT of reg2.
 # Stores the result in reg1.
-
-def invert():
-    return
+# C | opcode unused(5bits) reg1 reg2
+def invert(reg1, reg2, line_number):
+    return '01101' + '00000' + regFind(reg1) + regFind(reg2)
 
 
 # Compares reg1 and reg2 and sets up the FLAGS register.
-
-def compare():
-    return
+# C | opcode unused(5bits) reg1 reg2
+def compare(reg1, reg2, line_number):
+    return '01110' + '00000' + regFind(reg1) + regFind(reg2)
 
 
 # Jumps to mem_addr, where mem_addr is a memory address.
-
+# E | opcode unused(3bits) memory(8bits)
 def jmp():
-    return
+    return '01111'
 
 
 # Jump to mem_addr if the less than flagis set (less than
 # flag = 1), where mem_addr is a memory address.
-
+# E | opcode unused(3bits) memory(8bits)
 def jlt():
-    return
+    return '10000'
 
 
 # Jump to mem_addr if the greater than flag is set (greater than
 # flag = 1), where mem_addr is a memory address.
-
+# E | opcode unused(3bits) memory(8bits)
 def jgt():
-    return
+    return '10001'
 
 
 # Jump to mem_addr if the equal flag is set (equal flag =
 # 1), where mem_addr is a memory address.
-
+# E | opcode unused(3bits) memory(8bits)
 def je():
-    return
+    return '10010'
 
 
 # Stops the machine from executing until reset
-
+# F | opcode unused(11bits)
 def hlt():
-    return
+    return '10011' + '00000000000'
